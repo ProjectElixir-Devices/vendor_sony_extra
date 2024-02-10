b6f165da5e9c511e49076e9976d07002dba5b918
ahnet-69 <syedmusicrealise@gmail.com>
2024-02-10 12:35:14 +0500

    pdx206-extra: camera: Import Semc Stock sony camera

--- extra/camera/camera.mk
+++ extra/camera/camera.mk
@@ -4,6 +4,6 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.lens.oem_camera_package=com.sonymobile.photopro
+    ro.com.google.lens.oem_camera_package=com.sonyericsson.android.camera
 
 $(call inherit-product, vendor/sony/pdx206-extra/extra/camera/pdx206-camera-vendor.mk)
